149357b8ee54dfcc386946aca674bee736d974b6
sunshine <ziqingli@MacBook-Pro-62.local>
2020-04-02 00:25:37 -0400

    milestone1

--- app.js
+++ app.js
@@ -19,13 +19,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // body parser setup
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('search');
+  res.render('main');
 });
+app.get('/search', (req, res) => {
+    res.render('search');
+  });
+  app.get('/myInfo', (req, res) => {
+    res.render('info');
+  });
+  app.get('/myAppt', (req, res) => {
+    res.render('myAppt');
+  });
 
 app.listen(3000);
